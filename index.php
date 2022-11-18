bcc8f42dc96e9c22960af622d6ee67911100ff6d
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2022-11-18 01:32:48 +0100

    update files

---
+++ index.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pilotin - Accueil</title>
+    <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'>
+
+</head>
+<body>
+        <div class="tab">
+            <p>
+                Our products
+            </p>
+
+            <div class="tabs__items">
+                <div class="tabs__items__item">
+                    <i></i> Data factory <br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestiae porro
+                </div>
+
+                <div class="tabs__items__item">
+                    <i></i> Data factory <br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestiae porro
+                </div>
+            </div>
+        </div>
+
+
+        <div class="hero">
+
+            <nav class="nav">
+                <div class="nav__content">
+                    <a href="public/img/Logo_blanc.svg" class='logo'>
+                        img
+                    </a>
+                    <ul>
+                            <li>
+                                <a href="">
+                                Products
+                                </a>
+                            </li>
+
+                            <li>
+                                Ressources
+                            </li>
+
+                            <li>
+                                Company
+                            </li>
+                    </ul>
+
+                    <div class="buttons">
+                        <button class='btn btn-cyan'>
+                            Free demo
+                        </button>
+
+                        <p>
+                            FR
+                        </p>
+                    </div>
+                </div>
+            </nav>
+
+                <div class="hero__top">
+                    <p>
+                        Maintenance prevue le 17/08/2022 <span>
+                            Recevoir une alerte
+                        </span>
+                    </p>
+                </div>
+
+                <div class="hero__blocks">
+                    <div class="hero__blocks__block">
+                        <h1>
+                            Meet the future <br>
+                            of your <br>
+                            <span>organization</span> <br>
+                            today
+                        </h1>
+
+                        <p>
+                        We build software that empower public and private organizations
+                            to bridge the gap between data, decisions and strategy execution.
+                        </p>
+
+                        <button class='btn btn-orange'>
+                            Let's talk
+                        </button>
+
+                        <ul>
+                            <li>
+                                <img src="" alt="">
+                            </li>
+
+                            <li>
+                                img
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="hero__blocks__block">
+                        <img src="" alt="">
+                    </div>
+                </div>
+        </div>
+
+        <section>
+        <div class="about">
+            <div class="about__item">
+                <h2>
+                      Get out of the chaos
+                </h2>
+            </div>
+
+            <div class="about__item">
+                <p>
+                    In today’s perpetual disruption and digital transformation era, volume of data managed expand exponentially and change the way organizations do business. This comes as little surprise and induces a rising demand for data-driven customer experiences, together with changing data volume/sources and quality requirements.
+                    However, company runs their processes over hundreds of systems that don't play nice together, leaving a lot of room for inefficiencies to hide that are silent killers to your business performance.
+                    While many companies state they want to be data-driven, most have only begun to scratch the surface in terms of realizing the full potential of this rich resource.
+                    Pilot'in recognizes the right for organizations to become truly data-driven, moving  from a fragmented view to a global vision and drive their strategy even further.
+
+                </p>
+
+                <button class='btn btn-cyan'>
+                    Discover our vision
+                </button>
+            </div>
+        </div>
+        </section>
+
+        <section>
+        <div class="blue">
+            <img src="" alt="" class="blue__image">
+
+            <h2>
+            How we help tackle these <span>challenges</span> ?
+            </h2>
+
+            <div class="blue__item">
+                <div class="blue__item__box box-bg">
+                 </div>
+
+                <div class="blue__item__box">
+                    <div class="box-title">
+                        <img src="" alt="">
+                        <h2>DATAFACTORY</h2>
+                    </div>
+
+                    <p>
+                     Build your next generation data warehouse automation platform with its unique low-code/no-code approach.
+                     Accelerate your business today and generate untapped value from your organization's data.
+                    </p> <br>
+
+                    <ul>
+                        <li>
+                        Manage and orchestrate
+                        </li>
+
+                        <li>
+                        Dispatch powerd elements
+                        </li>
+                    </ul>
+
+                    <button class='btn btn-orange'>
+                        Decouvrir la solution
+                    </button>
+                </div>
+            </div>
+
+            <div class="blue__item">
+                <div class="blue__item">
+                        <div class="blue__item__box box-left">
+                        <div class="box-title">
+                            <img src="" alt="">
+                            <h2>PROPILOT</h2>
+                        </div>
+
+                    <p>
+                    Plan, do, check and align all your
+                    strategic plans in one single platform designed and
+                    built for modern and efficient organizations. No more spreadsheets
+                    sent via emails or time wasted for reconciliation. Focus on what really
+                    matters, achieving your goals.
+                    </p>
+
+                    <ul>
+                        <li><img src="" alt="">Connect strategic plans to execution</li>
+                        <li><img src="" alt="">Know the status of plans </li>
+                        <li> <img src="" alt=""> Adapt quickly to changes </li>
+                        <li><img src="" alt=""> Reward real achiviers</li>
+                    </ul>
+
+                    <button class='btn btn-orange'>
+                        Learn more about ProPilot
+                    </button>
+
+                        </div>
+                </div>
+            </div>
+        </div>
+        </section>
+
+        <section>
+            <div class="testimonials">
+                <h2>
+                They trust Pilot'in. <br>
+                <span> why not you? </span>
+                </h2>
+
+                <div class="testimonials__slider">
+                    <div class="video">
+
+                    </div>
+
+                    <div class="slide">
+                        <div class="slide1">
+                            EXX
+                        </div>
+
+                        <p class="author">
+                        Thierry Soubestre
+                        </p>
+
+                        <p class="description">
+                        In a few weeks, 5 critical
+                        financial data sources were integrated
+                        and information is now available every day.
+                        </p>
+
+                        <div class="buttons">
+                            <div class="button">
+                                ljil
+                            </div>
+
+                            <div class="button">
+                                bhs
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+        <div class="blog">
+            <h2>
+                Nos articles de blog
+            </h2>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Est ridiculus velit interdum aliquam libero sit a in. Id nibh vitae,
+                massa sed a vel ultricies pharetra auctor. Libero sed facilisi faucibus etiam aliquet amet, purus, tortor, cursus.
+                Mauris malesuada morbi ultrices diam amet fringilla adipiscing faucibus quis.
+            </p>
+
+            <div class="cards">
+                <div class="cards__card">
+                    <div class="category">
+                        Catégorie
+                    </div>
+
+                    <p class="extract extract-bold">
+                            Titre de l’article sur une ou plusieurs lignes.
+                            Au delà de 100 caractères le texte sera masqué par des poin…
+                    </p>
+
+                    <p class="extract">
+                            Titre de l’article sur une ou plusieurs lignes.
+                            Au delà de 100 caractères le texte sera masqué par des poin…
+                    </p>
+
+                    <div class="more">
+                        <img src="" alt="">
+                        <p class="blog-author">
+                        Julien Dereumaux - Capitaine de Pilot’in
+                        </p>
+
+                        <div class="btn">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cards__card">
+                    <div class="category">
+                        Catégorie
+                    </div>
+
+                    <p class="extract extract-bold">
+                            Titre de l’article sur une ou plusieurs lignes.
+                            Au delà de 100 caractères le texte sera masqué par des poin…
+                    </p>
+
+                    <p class="extract">
+                            Titre de l’article sur une ou plusieurs lignes.
+                            Au delà de 100 caractères le texte sera masqué par des poin…
+                    </p>
+
+                    <div class="more">
+                        <img src="" alt="">
+                        <p class="blog-author">
+                        Julien Dereumaux - Capitaine de Pilot’in
+                        </p>
+
+                        <div class="btn">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cards__card">
+                    <div class="category">
+                        Catégorie
+                    </div>
+
+                    <p class="extract extract-bold">
+                            Titre de l’article sur une ou plusieurs lignes.
+                            Au delà de 100 caractères le texte sera masqué par des poin…
+                    </p>
+
+                    <p class="extract">
+                            Titre de l’article sur une ou plusieurs lignes.
+                            Au delà de 100 caractères le texte sera masqué par des poin…
+                    </p>
+
+                    <div class="more">
+                        <img src="" alt="">
+                        <p class="blog-author">
+                        Julien Dereumaux - Capitaine de Pilot’in
+                        </p>
+
+                        <div class="btn">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button class='btn btn-cyan'>
+                Tous les articles
+            </button>
+        </div>
+        </section>
+
+        <section>
+            <div class="demo">
+                <div class="demo__icons">
+
+                </div>
+
+                <div class="demo__text">
+                    <h3>
+                    Ready to meet the future of your organization?
+                    </h3>
+
+                    <p>
+                    Begin your data-driven transformation today!
+                    </p>
+
+                    <button class='btn btn-orange'>
+                        Book a demo
+                    </button>
+                </div>
+            </div>
+        </section>
+
+    <footer class="footer">
+        <div class="footer__up">
+            <div class="footer__up__item">
+                <h4>
+                    Get involved
+                </h4>
+
+                <div class="links">
+                    <a href="">About us</a>
+                    <a href="">Join us</a>
+                    <a href="">Contact us</a>
+                </div>
+            </div>
+
+            <div class="footer__up__item">
+                <h4>
+                    Belgium
+                </h4>
+
+                <div class="links">
+                    <p>Nom et numéro de la voie</p>
+                    <span> +33 00000000</span>
+                </div>
+            </div>
+
+            <div class="footer__up__item">
+                <h4>
+                    Belgium
+                </h4>
+
+                <div class="links">
+                    <p>Nom et numéro de la voie</p>
+                    <span> +33 00000000</span>
+                </div>
+            </div>
+
+            <div class="footer__up__item">
+                <p>
+                Suivez nous sur les réseaux  sociaux
+                </p>
+
+                <div class="links">
+                    <div class="link">
+                        <img src="" alt="">
+                    </div>
+
+                    <div class="link">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__bottom">
+            <ul>
+                <li>Pilot'in 2022 - CGV -</li>
+                <li>- Cookies -</li>
+                <li>Mentions légales</li>
+            </ul>
+
+            <p>
+            Mis à flot par <span>Pilot'in</span>
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
